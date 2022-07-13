1cb1ecb482f32b771c78ef59a567e1d52c48accb
Tuyishimire Eric~ <tuyishimireericc@gmail.com>
2022-07-13 09:17:15 +0200

    fix eslinters

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import './style.css';
 
-let todoList = [];
+function showTodo(todo) {
+  const list = document.querySelector('.list');
+  const newlist = document.createElement('li');
+  newlist.classList.add('newlist');
+  newlist.innerHTML = `
+      <input id="${todo.index}" class="check" type="checkbox"/>
+      <span class="text">${todo.description}</span>
+      <button class="delete"><img src="../images/dots.svg"></button>
+    `;
+  list.append(newlist);
+}
+
+const todoList = [];
 let id = 1;
 function add(text) {
-  
   const todo = {
     description: text,
     completed: false,
-    index: id ++,
+    index: id += 1,
   };
   todoList.push(todo);
   showTodo(todo);
 }
 
 const form = document.querySelector('.main');
-form.addEventListener('submit', event => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   const input = document.querySelector('.action');
   const text = input.value.trim();
@@ -25,21 +36,7 @@ form.addEventListener('submit', event => {
   }
 });
 
-function showTodo(todo) {
-    const list = document.querySelector('.list');
-    const newlist = document.createElement("li");
-    newlist.classList.add("newlist");
-    newlist.innerHTML = `
-      <input id="${todo.index}" class="check" type="checkbox"/>
-      <span class="text">${todo.description}</span>
-      <button class="delete"><img src="../images/dots.svg"></button>
-    `;
-    list.append(newlist);
-}
-
-
- const completed = document.querySelector('.newlist');
- completed.addEventListener('click', event => {
-    completed.classList.toggle('complete');
- }
-)
+const completed = document.querySelector('.newlist');
+completed.addEventListener('click', () => {
+  completed.classList.toggle('complete');
+});
