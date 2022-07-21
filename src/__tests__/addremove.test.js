fce0a101377b170cf3bf7b47f4c1ddca079ee049
Tuyishimire Eric~ <tuyishimireericc@gmail.com>
2022-07-21 16:40:20 +0200

    clear completed test

--- src/__tests__/addremove.test.js
+++ src/__tests__/addremove.test.js
@@ -1,6 +1,7 @@
 /**
  * @jest-environment jsdom
  */
+/* eslint-disable no-unused-vars */
 document.body.innerHTML = `
     <div class="row">
             <ul class="todo-list">
@@ -11,6 +12,7 @@ let Array;
 let rest;
 let updated;
 let completed;
+let unchecked;
 
 function newTodo(e) {
   const newTask = e;
@@ -37,11 +39,12 @@ describe('By adding item in todos', () => {
   newTodo('finish project');
   newTodo('submit project');
   newTodo('done');
+  newTodo('start part 2');
   test('check if newTodo is a function', () => {
     expect(typeof newTodo).toBe('function');
   });
   test('test if newTodo is adding', () => {
-    expect(Array).toBe(3);
+    expect(Array).toBe(4);
   });
 });
 
@@ -53,7 +56,7 @@ describe('By removing item in todos', () => {
     expect(typeof removeTodo).toBe('function');
   });
   test('test if todos are removed from array', () => {
-    expect(rest).toBe(2);
+    expect(rest).toBe(3);
   });
 });
 
@@ -75,21 +78,20 @@ function toggleComplete(id) {
   const todoListArray = JSON.parse(localStorage.getItem('todos') || '[]');
   const updateTodoList = todoListArray.map((todo) => {
     if (todo.id === parseInt(id, 10)) {
+      completed = !todo.completed;
       return { ...todo, completed: !todo.completed };
     }
     return todo;
   });
-
-  updateTodos(updateTodoList);
 }
 
 const removeCompleted = () => {
   const todoListArr = JSON.parse(localStorage.getItem('todos') || '[]');
   const updateList = todoListArr.filter((todo) => todo.completed !== true);
-  updateTodos(updateList);
+  unchecked = updateList.length;
 };
 
-describe('By removing item in todos', () => {
+describe('By updating in todos description', () => {
   // Action with index 1
   updateInputText(1, 'updated text');
   // Assert
@@ -103,12 +105,24 @@ describe('By removing item in todos', () => {
 
 describe('By checking the complete status of todos', () => {
   // Action with index 1
-  toggleComplete(1);
+  toggleComplete(3);
   // Assert
   test('check if toggleComplete is a function', () => {
     expect(typeof toggleComplete).toBe('function');
   });
   test('test if todos are completed from array', () => {
-    expect(updated).toBe(true);
+    expect(completed).toBe(true);
+  });
+});
+
+describe('By removing all completed item in todos', () => {
+  // Action with index 1
+  removeCompleted();
+  // Assert
+  test('check if removeTodo is a function', () => {
+    expect(typeof removeCompleted).toBe('function');
+  });
+  test('test if todos are removed from array', () => {
+    expect(unchecked).toBe(4);
   });
 });
